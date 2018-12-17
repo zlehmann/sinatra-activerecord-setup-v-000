2f0c8a405396a0d23df90d1ba412477ef21315ed
Zachary Lehmann <zlehmann@gmail.com>
2018-12-17 21:09:06 +0000

    Automatically backed up by Learn

--- db/migrate/20181217210746_create_dogs.rb
+++ db/migrate/20181217210746_create_dogs.rb
@@ -1,8 +1,8 @@
 class CreateDogs < ActiveRecord::Migration
   def up
     create_table :dogs do |t|
-      t.string :name 
-      t.string :breed 
+      t.string :name
+      t.string :breed
     end
   end
 
